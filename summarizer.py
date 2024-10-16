2c3d898fc42faa012834c694e601fe0a56bc8c6b
D4NGK4 <danjosheps.djt98@gmail.com>
2024-10-16 23:18:40 +0800

    test

--- summarizer.py
+++ summarizer.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+client = OpenAI(api_key=os.environ.get('API_KEY'))
 
 def fix_summary(paragraph: str):
     prompt = (
